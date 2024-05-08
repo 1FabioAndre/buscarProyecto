dece9cfde4012e1fe14a4b37e2852bdf927318cd
Fabio Andre <fabio.mojica@ucb.edu.bo>
2024-05-08 18:20:46 -0400

    Encuentra mas de una coincidencia

--- src/buscar.js
+++ src/buscar.js
@@ -1,10 +1,11 @@
 function buscarProyecto(proyecto="", array){
+    let coincidencias = [];
     for(let i = 0; i < array.length; i++){
         if(proyecto === array[i]){
-            return array[i];
+            coincidencias.push(array[i]);
         }
     }
-    return "";
+    return coincidencias;
 }
 
 export default buscarProyecto;
--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -6,13 +6,23 @@ import buscarProyecto from "./buscar";
 describe("Buscar", () => {
     it("no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => { 
         let array = [];
-        expect(buscarProyecto("proyecto1", array)).toEqual("");
+        expect(buscarProyecto("proyecto1", array)).toEqual([]);
     });
 
     // Encontrando un proyecto en una lista que solo contiene un proyecto
     it("Encontrar un proyecto en una lista que solo contiene un proyecto", () => { 
         let array = [];
         array.push("Proyecto1")
-        expect(buscarProyecto("Proyecto1", array)).toEqual("Proyecto1");
+        expect(buscarProyecto("Proyecto1", array)).toEqual(["Proyecto1"]);
+    });
+
+    // Encontrando mas de una coincidencia
+    it("buscar y encontrar mas de una coincidencia", () => { 
+        let array = [];
+        array.push("Proyecto1");
+        array.push("Proyecto2");
+        array.push("Proyecto2");
+        array.push("Proyecto3");
+        expect(buscarProyecto("Proyecto2", array)).toEqual(["Proyecto2", "Proyecto2"]);
     });
 });
